0d526b36f8d2ac0b0c6d2034888bbbe3a9fc802b
iAmAim <schnider_17@yahoo.com>
2016-09-21 23:18:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
    var currentElem = $( this ); 
    console.log( "currentElem:", currentElem.text() );
     if(clickedElem.text() === currentElem.text()){
-     console.log("Clicked elem is the current, removing active class")
-    currentElem.addClass( "active" );
+     console.log("Clicked elem is the current, adding active class")
+    clickedElem.addClass( "active" );
     console.log(currentElem);
     }else{
      console.log("removing class active")
