f54e51f90049533ddccb48bf714ef2b340749e33
iAmAim <schnider_17@yahoo.com>
2016-09-21 22:48:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-$(document).ready(function() 
- { console.log("hadouken!");
-    $('li').click(function(e) 
-    { 
-     var clickedElem= $(this).text(); 
-      $( "li" ).each(function( index ) {
-       
-       var currentElem = $( this ).text();
-       console.log( index + ": " + currentElem );
-      
-       if(clickedElem === currentElem){
-        $(this).addClass( "active" );
-       }
-    });
-
-    });
+$(document).ready(function() { 
+ console.log("hadouken!");
+ 
  });
