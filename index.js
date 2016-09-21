2bd248d2ef2081bbc670b5df6d2d7f52812d3f3d
iAmAim <schnider_17@yahoo.com>
2016-09-21 22:55:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() { 
  $('li').click(function(e) { 
    var clickedElem= $(this).text();
+    console.log( clickedElem);
    $( "li" ).each(function( index ) {
    var currentElem = $( this ).text(); 
    console.log( index + ": " + currentElem );
