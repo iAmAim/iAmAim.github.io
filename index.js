f44fc118d01ecd6c3dc2fa77e0b288c979b43016
iAmAim <schnider_17@yahoo.com>
2016-09-21 23:02:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 $(document).ready(function() { 
  $('li').click(function(e) { 
    var clickedElem= $(this);
-    console.log( clickedElem.text());
+    console.log( "clickedElem: "clickedElem.text());
    $( "li" ).each(function( index ) {
    var currentElem = $( this ); 
-   console.log( "current element", currentElem.text() );
+   console.log( "currentElem:", currentElem.text() );
     if(clickedElem.text() === currentElem.text()){
+     console.log("Clicked elem is the current, removing active class")
     //  currentElem.addClass( "active" );
     currentElem.removeClass( "active" );
+    console.log(currentElem);
     }else{
       currentElem.removeClass( "active" );
     }
