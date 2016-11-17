b965ec8e95a895142b405658905f64f1012b1496
iAmAim <schnider_17@yahoo.com>
2016-11-17 17:12:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
  setDefaultLocation();
  
  //add click event handler for the menu
- $('.menuItem').click(displayMenuContent.bind(this));
+ $('.menuItem').bind("click",displayMenuContent());
  
 });
 
